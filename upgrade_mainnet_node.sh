500c27917ca78d75c8090e4e8c9be761cae52b77
VALIDEXIS <validexisglobal@gmail.com>
2025-05-15 20:53:41 +0300

    Create upgrade_mainnet_node.sh

---
+++ upgrade_mainnet_node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo systemctl stop celestia-bridge
+cd $HOME
+rm -rf celestia-node
+git clone https://github.com/celestiaorg/celestia-node.git
+cd celestia-node/
+git checkout tags/v0.22.2 
+make build 
+sudo make install 
+make cel-key
+celestia bridge config-update
+sudo systemctl restart celestia-bridge && sudo journalctl -u celestia-bridge -f
