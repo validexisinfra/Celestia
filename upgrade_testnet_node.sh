0ef913c6b1fb4d801f031a6bcb69074e3e3480a7
VALIDEXIS <validexisglobal@gmail.com>
2025-05-15 20:52:13 +0300

    Create upgrade_testnet_node.sh

---
+++ upgrade_testnet_node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo systemctl stop celestia-bridge
+cd $HOME
+rm -rf celestia-node
+git clone https://github.com/celestiaorg/celestia-node.git
+cd celestia-node/
+git checkout tags/v0.22.2-mocha 
+make build 
+sudo make install 
+make cel-key
+celestia bridge config-update --p2p.network mocha
+systemctl restart celestia-bridge && journalctl -u celestia-bridge -f -o cat
