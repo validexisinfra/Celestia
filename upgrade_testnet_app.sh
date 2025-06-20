82801f25b23b1a5bc912484a6f403fcc4e314367
VALIDEXIS <validexisglobal@gmail.com>
2025-06-21 00:38:09 +0300

    Update upgrade_testnet_app.sh

--- upgrade_testnet_app.sh
+++ upgrade_testnet_app.sh
@@ -6,4 +6,5 @@ cd celestia-app/
 APP_VERSION=v4.0.3-mocha
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
+sed -i -e "s%^proxy_app *=.*%proxy_app = \"tcp://127.0.0.1:36658\"%" $HOME/.celestia-app/config/config.toml
 sudo systemctl restart celestia-appd && sudo journalctl -u celestia-appd -f
