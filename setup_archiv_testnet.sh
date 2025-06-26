48246fd75bc9166fba4dc9d9c4e3eba89e305b55
VALIDEXIS <validexisglobal@gmail.com>
2025-06-26 19:41:14 +0300

    Update setup_archiv_testnet.sh

--- setup_archiv_testnet.sh
+++ setup_archiv_testnet.sh
@@ -43,7 +43,7 @@ cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
 cd celestia-app
-git checkout v4.0.3-mocha
+git checkout v4.0.6-mocha
 make install
 
 print "=== Configuring node ==="
