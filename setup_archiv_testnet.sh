483bc2b911f18add7c1e28d21211aff3a88c06ea
VALIDEXIS <validexisglobal@gmail.com>
2025-07-11 12:04:39 +0300

    Update setup_archiv_testnet.sh

--- setup_archiv_testnet.sh
+++ setup_archiv_testnet.sh
@@ -31,7 +31,7 @@ sudo apt install -y curl git wget htop tmux build-essential jq make lz4 gcc unzi
 
 print "=== Installing Go ==="
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.24.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.24.2.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh > /dev/null
 echo 'export PATH=$PATH:$HOME/go/bin' >> $HOME/.profile
 echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
@@ -112,7 +112,7 @@ WantedBy=multi-user.target
 EOF
 
 print "=== Downloading chain snapshot ==="
-curl -L https://snapshots1.validexis.com/celestia-testnet/snap_celestia-archive_6994876.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app/
+curl -L https://snapshots1.validexis.com/celestia-testnet/snap_celestia-archive_7060225.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app/
 
 print "=== Starting Celestia node ==="
 sudo systemctl daemon-reload
