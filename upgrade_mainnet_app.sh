f8cbad4c43ad5e790e858d70dde859c38118fa14
VALIDEXIS <validexisglobal@gmail.com>
2025-07-31 12:06:04 +0300

    Update upgrade_mainnet_app.sh

--- upgrade_mainnet_app.sh
+++ upgrade_mainnet_app.sh
@@ -2,12 +2,7 @@
 rm -rf $HOME/celestia-app
 git clone https://github.com/celestiaorg/celestia-app/
 cd $HOME/celestia-app
-git checkout v4.0.10
+git checkout v5.0.1
 make install
-
-sed -i \
-  -e 's|^grpc_laddr *=.*|grpc_laddr = "tcp://127.0.0.1:9098"|' \
-  -e 's|^proxy_app *=.*|proxy_app = "tcp://127.0.0.1:36658"|' \
-  $HOME/.celestia-app/config/config.toml
 ​
 sudo systemctl restart celestia-appd && sudo journalctl -fu celestia-appd -o cat
