a5ca8cb2cced1dda44dbc455f35e796885d72460
VALIDEXIS <validexisglobal@gmail.com>
2025-08-18 17:15:21 +0300

    Update upgrade_mainnet_app.sh

--- upgrade_mainnet_app.sh
+++ upgrade_mainnet_app.sh
@@ -2,7 +2,7 @@
 rm -rf $HOME/celestia-app
 git clone https://github.com/celestiaorg/celestia-app/
 cd $HOME/celestia-app
-git checkout v5.0.1
+git checkout v5.0.2
 make install
 ​
 sudo systemctl restart celestia-appd && sudo journalctl -fu celestia-appd -o cat
