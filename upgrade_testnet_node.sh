38677730a84f7c964c2793ad128887131e3deb60
VALIDEXIS <validexisglobal@gmail.com>
2025-08-18 20:08:57 +0300

    Update upgrade_testnet_node.sh

--- upgrade_testnet_node.sh
+++ upgrade_testnet_node.sh
@@ -4,7 +4,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.24.1-mocha 
+git checkout tags/v0.25.2-mocha 
 make build 
 sudo make install 
 make cel-key
