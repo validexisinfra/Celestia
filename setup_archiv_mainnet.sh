e5b1ca9a6f4506785bfa67e776ced134077ad29e
VALIDEXIS <validexisglobal@gmail.com>
2025-08-23 13:35:49 +0300

    Update setup_archiv_mainnet.sh

--- setup_archiv_mainnet.sh
+++ setup_archiv_mainnet.sh
@@ -111,7 +111,7 @@ WantedBy=multi-user.target
 EOF
 
 print "=== Downloading chain snapshot ==="
-curl -L https://snapshots1.validexis.com/celestia-mainnet/snap_celestia-archive_6932261.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app/
+curl -L https://snapshots1.validexis.com/celestia-mainnet/snap_celestia-archive_7085871.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app/
 
 print "=== Starting Celestia node ==="
 sudo systemctl daemon-reload
