e0887e556cd10dceb1c21034580fe24f8d5bf813
VALIDEXIS <validexisglobal@gmail.com>
2025-09-15 12:01:11 +0300

    Update upgrade_testnet_app.sh

--- upgrade_testnet_app.sh
+++ upgrade_testnet_app.sh
@@ -3,7 +3,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v5.0.2-mocha
+APP_VERSION=v5.0.5-mocha
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 
