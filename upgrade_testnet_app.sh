36874efa7a5378203a2e87643410fc25357dfac2
VALIDEXIS <validexisglobal@gmail.com>
2025-09-25 18:56:19 +0300

    Update upgrade_testnet_app.sh

--- upgrade_testnet_app.sh
+++ upgrade_testnet_app.sh
@@ -3,8 +3,8 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v5.0.5-mocha
+APP_VERSION=v6.0.5-mocha
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
-
+celestia-appd update-config
 sudo systemctl restart celestia-appd && sudo journalctl -u celestia-appd -f
