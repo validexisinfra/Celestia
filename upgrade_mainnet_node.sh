f4f210f591f87c88cee18e394fabb95013a49224
VALIDEXIS <validexisglobal@gmail.com>
2025-10-07 18:55:33 +0300

    Update upgrade_mainnet_node.sh

--- upgrade_mainnet_node.sh
+++ upgrade_mainnet_node.sh
@@ -4,7 +4,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.25.3 
+git checkout tags/v0.26.4 
 make build 
 sudo make install 
 make cel-key
