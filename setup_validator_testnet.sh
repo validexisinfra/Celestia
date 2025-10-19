28e50fd351ccf33ebb9d872a7308347a54fed0bc
VALIDEXIS <validexisglobal@gmail.com>
2025-10-19 16:20:26 +0300

    Update setup_validator_testnet.sh

--- setup_validator_testnet.sh
+++ setup_validator_testnet.sh
@@ -31,7 +31,7 @@ sudo apt install -y curl git wget htop tmux build-essential jq make lz4 gcc unzi
 
 print "=== Installing Go ==="
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.24.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.24.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh > /dev/null
 echo 'export PATH=$PATH:$HOME/go/bin' >> $HOME/.profile
 echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
