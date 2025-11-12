cf2de0b00c4942ed9aeac11f107e9d6c4ed079a7
VALIDEXIS <validexisglobal@gmail.com>
2025-11-12 20:26:36 +0200

    Update setup_archiv_mainnet.sh

--- setup_archiv_mainnet.sh
+++ setup_archiv_mainnet.sh
@@ -43,7 +43,7 @@ cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
 cd celestia-app
-git checkout v5.0.11
+git checkout v6.2.4
 make install
 
 print "=== Configuring node ==="
