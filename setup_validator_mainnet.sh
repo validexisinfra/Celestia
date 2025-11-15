bfe819235f27b87eed734dec15eba9e8b924c060
VALIDEXIS <validexisglobal@gmail.com>
2025-11-15 21:01:19 +0200

    Update setup_validator_mainnet.sh

--- setup_validator_mainnet.sh
+++ setup_validator_mainnet.sh
@@ -43,7 +43,7 @@ cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
 cd celestia-app
-git checkout v6.2.4
+git checkout v6.2.5
 make install
 
 print "=== Configuring node ==="
