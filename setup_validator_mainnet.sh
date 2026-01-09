49f9b6dfa55d332fe0555b1339e60016c6404847
VALIDEXIS <validexisglobal@gmail.com>
2026-01-09 10:31:40 +0200

    Update celestia-app version to v6.4.4

--- setup_validator_mainnet.sh
+++ setup_validator_mainnet.sh
@@ -43,7 +43,7 @@ cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
 cd celestia-app
-git checkout v6.4.2
+git checkout v6.4.4
 make install
 
 print "=== Configuring node ==="
