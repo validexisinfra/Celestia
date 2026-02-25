ca3332f2d77ecf58451df6cc51b9df24fa1f0f10
VALIDEXIS <validexisglobal@gmail.com>
2026-02-25 20:47:18 +0200

    Update git checkout version for celestia-app

--- setup_validator_testnet.sh
+++ setup_validator_testnet.sh
@@ -43,7 +43,7 @@ cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
 cd celestia-app
-git checkout v6.4.10-mocha
+git checkout v7.0.2-mocha
 make install
 
 print "=== Configuring node ==="
